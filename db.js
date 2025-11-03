590adbc38bc33059d07bbd712d6cbfc6af168bab
not-asim04 <mr.asim881@gmail.com>
2025-11-03 14:25:55 +0500

    shift mognodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 //define the mongodb url connection string
-const mongoURL = 'mongodb://localhost:27017/hotels'; //Replace "hotels" with your database name
+//const mongoURL = process.env.DB_URL_LOCAL; //Replace "hotels" with your database name
+const mongoURL = process.env.DB_URL;
 
 //Set up mongodb connection
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
